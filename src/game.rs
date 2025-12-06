60c448b1dbba964270bfdc696b701a9fbfd376d3
Paulo Henrique Soares <phsoares.ita@gmail.com>
2025-12-06 19:22:37 -0300

    validating number

--- src/game.rs
+++ src/game.rs
@@ -9,7 +9,7 @@ pub fn initialize() -> i32 {
 
 pub fn run_main_loop(secret_number: i32) {
     loop {
-        let mut number_guess: i32 = read_user_input();
+        let number_guess: i32 = read_user_input();
         match number_guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
@@ -22,22 +22,17 @@ pub fn run_main_loop(secret_number: i32) {
 }
 
 fn get_random_number() -> i32 {
-    let secret_number = rand::rng().random_range(1..=100);
-    secret_number
+    rand::rng().random_range(1..=100)
 }
 
 fn read_user_input() -> i32 {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
-    input.trim().parse().expect("Please type a number!")
-}
-
-fn evaluate_guess(guess: i32, secret: i32) {
-    if guess < 0 {
-        println!("Please enter a valid number between 1 and 20.");
-    } else if guess < secret {
-        println!("Too small!");
-    } else if guess > secret {
-        println!("Too big!");
+    match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a valid number.");
+            read_user_input()
+        }
     }
 }
